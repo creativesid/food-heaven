23f7e8b5ee6938fe5dbbaa8a9a667bb385d2507c
siddharth pandey <56196602+creativesid@users.noreply.github.com>
2021-10-04 17:00:51 +0530

    Update constant.php

--- config/constant.php
+++ config/constant.php
@@ -2,10 +2,10 @@
 
     session_start();
     define('HOMEURL', 'https://food-heaven.herokuapp.com/');
-    define('LOCALHOST', 'sql6.freemysqlhosting.net');
-    define('DBUSER', 'sql6441289');
-    define('DBPASS', 'MlWneeCZuI');
-    define('DBNAME', 'sql6441289');
+    define('LOCALHOST', 'remotemysql.com');
+    define('DBUSER', 'QSnXffpEec');
+    define('DBPASS', '1bUgQvBVFb');
+    define('DBNAME', 'QSnXffpEec');
     
     $conn = mysqli_connect(LOCALHOST,DBUSER,DBPASS) or die('failed');
     $db_select = mysqli_select_db($conn,DBNAME) or die('failed');
